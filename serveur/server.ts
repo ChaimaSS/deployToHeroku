9154c462c537ea5623c8faa28a492a74e412b6de
ChaimaSS <chaima.soussi@hotmail.com>
2022-02-09 17:49:01 -0500

    add view

--- serveur/server.ts
+++ serveur/server.ts
@@ -6,8 +6,8 @@ const app = express();
 var PORT = process.env.PORT || 3000;
 const server = app.listen(PORT); 
 
-//app.set('views', path.join(__dirname, 'views'))
-//app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 
 app.use(express.static('public')); 
