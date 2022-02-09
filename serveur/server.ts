2e9bf14001c8758a80d39da90eb3374de6ba9c31
ChaimaSS <chaima.soussi@hotmail.com>
2022-02-09 17:41:14 -0500

    modif server

--- serveur/server.ts
+++ serveur/server.ts
@@ -1,12 +1,13 @@
 const express = require('express'); 
-const socket = require('socket.io'); 
+const socket = require('socket.io');
+const path = require('path')
 const fs = require('fs');
 const app = express();
 var PORT = process.env.PORT || 3000;
 const server = app.listen(PORT); 
 
-//app.set('views', path.join(__dirname, 'views'))
-//app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
 
 app.use(express.static('public')); 
